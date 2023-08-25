ea775e60ed8693ce0a0acbc9acd059ccf7aec741
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-08-25 16:37:03 -0300

    Template de E-mail Personalizado

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Email-Django
 Repositório criado para testes de envio de e-mails com Python e Django.
 
-Obs: para utilizar esse código, adicione as informações do e-mail do remetente dentro dos arquivos .env e Settings.py.
+Obs: para utilizar esse código, adicione as informações do e-mail do remetente dentro dos arquivos .env, views.py e Settings.py.
--- enviaemail/views.py
+++ enviaemail/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.conf import settings
 
 def envia_email(request):
-    send_mail('Assunto', 'Esse é o e-mail que estou te enviando','avelinogabrieldossantos@gmail.com', ['aluno103.23187221gabriel@gmail.com'])
+    # send_mail('Assunto', 'Esse é o e-mail que estou te enviando','<remetente>', ['<destinatários>'])
+
+    html_content = render_to_string('emails/cadastro_confirmado.html', {'nome': 'Gabriel'})
+    text_content = strip_tags(html_content)
+
+    email = EmailMultiAlternatives('Cadastro Confirmado', text_content,settings.EMAIL_HOST_USER, ['<destinatários>'])
+    email.attach_alternative(html_content, 'text/html')
+    email.send()
     return HttpResponse('Olá')
